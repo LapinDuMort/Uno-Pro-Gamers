28d39504b249eb54456ebc3621f4fce16b120838
Blake Harris <dev.blake.harris@gmail.com>
2025-12-09 10:35:13 +0000

    CHORE: project scaffold

---
+++ src/main/java/com/progamers/uno/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.progamers.uno.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index"; // returns the name of the view
+    }
+}
