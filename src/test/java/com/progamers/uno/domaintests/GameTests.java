f3015edafe29a41a7a0688c9f77dbf8b57e758d2
Blake Harris <dev.blake.harris@gmail.com>
2025-12-10 11:24:54 +0000

    Adds "GameTests" test class to test drawing a starting hand ("testDrawAStartingHand")

---
+++ src/test/java/com/progamers/uno/domaintests/GameTests.java
@@ -0,0 +1,44 @@
+package com.progamers.uno.domaintests;
+
+import com.progamers.uno.PlayerController;
+import com.progamers.uno.domain.Card;
+import com.progamers.uno.domain.Colour;
+import com.progamers.uno.domain.Deck;
+import com.progamers.uno.domain.Value;
+import com.progamers.uno.domain.game.Game;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class GameTests {
+
+    Game game;
+    PlayerController player;
+
+    @BeforeEach
+    void setup() {
+        game = new Game();
+        player = new PlayerController();
+    }
+
+    @Test
+    void testDrawAStartingHand() {
+        game.drawCards(this.player, 7);
+        assert player.getHandSize() == 7;
+
+        Deck deck = game.getCardDeck();
+        assert deck.getDeckSize() == 101;
+
+        Card res = Card.builder().colour(Colour.Red).value(Value.Zero).build();
+
+        try {
+            assert player.getCurrentSelectedCard(0).equals(res);
+        }
+        catch (Exception e) {
+            fail();
+        }
+    }
+}
