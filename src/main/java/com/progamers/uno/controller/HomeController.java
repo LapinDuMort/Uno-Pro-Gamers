a516b2bffa18bb6958791c293c7de96917de8f71
Aryn McDowell <arynmcdowell17@gmail.com>
2025-12-11 10:14:16 +0000

    FEAT: enhance HomeController with card play functionality and update UnoPlayerPage for dynamic card display

--- src/main/java/com/progamers/uno/controller/HomeController.java
+++ src/main/java/com/progamers/uno/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.progamers.uno.controller;
 
+import com.progamers.uno.PlayerController;
 import com.progamers.uno.domain.Card;
 import com.progamers.uno.domain.Colour;
 import com.progamers.uno.domain.Deck;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.ArrayList;
@@ -18,15 +20,17 @@ public class HomeController {
 
     private ArrayList<Card> playerHand = new ArrayList<>();
     private Deck deck = new StandardDeckFactory().createDeck();
+    private PlayerController playerController = new PlayerController();
+    private Card discardCard = Card.builder().colour(Colour.Red).value(Value.Five).build();
 
     public HomeController() {
+        deck.shuffle();
 
         // Deal 7 cards to the player
         for (int i = 0; i < 7; i++) {
             playerHand.add(deck.drawCard());
         }
     }
-
 
     @GetMapping("/")
     public String index() {
@@ -37,7 +41,7 @@ public class HomeController {
     public String ViewPlayer(Model model) {
 
         model.addAttribute("playerHand", playerHand);
-        model.addAttribute("discardCard", Card.builder().colour(Colour.Red).value(Value.Five).build());
+        model.addAttribute("discardCard", discardCard);
         return "UnoPlayerPage";
     }
 
@@ -45,6 +49,16 @@ public class HomeController {
     public RedirectView drawCard() {
 
         playerHand.add(deck.drawCard());
+        return new RedirectView("/playerpage");
+    }
+
+    @PostMapping("/play")
+    public RedirectView playCard(@RequestParam("cardIndex") int cardIndex, Model model) {
+        Card playedCard = playerHand.remove(cardIndex);
+        discardCard = playedCard;
+
+        model.addAttribute("playerHand", playerHand);
+        model.addAttribute("discardCard", discardCard);
         return new RedirectView("/playerpage");
     }
 }
