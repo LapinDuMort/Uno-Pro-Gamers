47587e2f9e5dc44d68d20c074213e7e94a203263
Robyn Archer <robynjarcher96@gmail.com>
2025-12-11 14:07:31 +0000

    Rebasing branch

---
+++ src/main/java/com/progamers/uno/domain/Player.java
@@ -0,0 +1,88 @@
+package com.progamers.uno.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@Getter
+@Setter
+public class Player {
+
+    private Boolean hasUno;
+    private ArrayList<Card> playerHand;
+    private int currentSelected;
+
+    public Player() {
+        this.hasUno = false;
+        this.playerHand = new ArrayList<Card>();
+        this.currentSelected = 0;
+    }
+    public Card getCurrentSelectedCard(int index) throws Exception {
+        // Check if the player's hand is empty
+        if (playerHand.isEmpty()) {
+            throw new Exception("Player hand is empty");
+        }
+        // Check if the index is within bounds
+        if (index < 0 || index >= playerHand.size()) {
+            throw new Exception("Index is out of bounds");
+        }
+        return(playerHand.get(index));
+    }
+
+    public Card playCard(int index){
+        // Implementation for playing a card from the player's hand
+        //get gamestate
+        //use gamestate to check card against ruleset
+        //if valid pop card from hand and gamesate ends player turn
+        return playerHand.remove(index);
+    }
+
+    @Deprecated
+    public boolean drawStartingHand(int HandSize){
+        // Drawing the starting hand
+        // Will be removed later and drawCard will be called externally to pass
+        // a card from a deck to the player
+        for(int i = 0; i < HandSize; i++)
+        {
+            addCardToHand(Card.builder().colour(Colour.Red).value(Value.getValue(i)).build());
+        }
+        return true;
+    }
+
+    public void addCardToHand (Card card) {
+        // Adds a given card to the player's hand
+        // Done this way so that hasUno can be unset
+        playerHand.add(card);
+        if (playerHand.size() > 1) {
+            hasUno = false;
+        }
+    }
+
+    public void DeclareUno() {
+        // Implementation for declaring UNO
+        if (playerHand.size() == 1)
+        {
+            hasUno = true;
+        }
+        else
+        {
+            hasUno = false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Player" + "playerHand=" + playerHand + ", currentSelected=" + currentSelected + '}';
+    }
+
+    public void showHand() {
+        for (int i = 0; i < playerHand.size(); i++) {
+            System.out.printf("%d: %s %s%n", i + 1, playerHand.get(i).getColour(), playerHand.get(i).getValue());
+        }
+    }
+
+    public int getHandSize() {
+        return playerHand.size();
+    }
+}
