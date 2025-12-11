f98105eab45b25e08188b09257e13805b87f332f
Blake Harris <dev.blake.harris@gmail.com>
2025-12-11 19:29:50 +0000

    Adds simple HomeControllerTests.java for coverage

---
+++ src/test/java/com/progamers/uno/controller/HomeControllerTests.java
@@ -0,0 +1,29 @@
+package com.progamers.uno.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.containsString;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    /* --- GET / --- */
+
+    @Test
+    void testIndex_whenVisited_thenDisplaysMainMenu() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home/index"))
+                .andExpect(content().string(containsString("<h1>UNO!</h1>")));
+
+    }
+}
