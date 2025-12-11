ac67515f711bfddecd2152dd5ac6fda2ccf4ff98
Robyn Archer <robynjarcher96@gmail.com>
2025-12-11 14:07:31 +0000

    Fixing CRLF

---
+++ src/main/java/com/progamers/uno/controller/GameController.java
@@ -0,0 +1,56 @@
+package com.progamers.uno.controller;
+
+import com.progamers.uno.PlayerController;
+import com.progamers.uno.domain.game.Game;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class GameController {
+
+    Game Mygame;
+    PlayerController MyplayerController;
+    public HomeController() {
+        Mygame = new Game();
+        Mygame.getCardDeck().shuffle();
+        Mygame.getDiscardPile().addToPile(Mygame.getCardDeck().drawCard());
+        MyplayerController = new PlayerController();
+
+        Mygame.drawCards(MyplayerController, 7);
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index"; // returns the name of the view
+    }
+
+    @GetMapping("/playerpage")
+    public String ViewPlayer(Model model) {
+
+        model.addAttribute("playerHand", MyplayerController.getPlayerHand());
+        model.addAttribute("discardCard", Mygame.getDiscardPile().getTopCard());
+        return "UnoPlayerPage";
+    }
+
+    @PostMapping("/draw")
+    public RedirectView drawCard() {
+
+        Mygame.drawCards(MyplayerController, 1);
+        return new RedirectView("/playerpage");
+    }
+
+    @PostMapping("/play")
+    public RedirectView playCard(@RequestParam("cardIndex") int cardIndex, Model model) throws Exception {
+
+        Mygame.getDiscardPile().addToPile(MyplayerController.getCurrentSelectedCard(cardIndex));
+        Mygame.getDiscardPile().addToPile(MyplayerController.playCard(cardIndex));
+
+        model.addAttribute("playerHand", MyplayerController.getPlayerHand());
+        model.addAttribute("discardCard", Mygame.getDiscardPile().getTopCard());
+        return new RedirectView("/playerpage");
+    }
+}
---
+++ src/test/java/com/progamers/uno/PlayerTests.java
@@ -0,0 +1,129 @@
+package com.progamers.uno;
+
+import com.progamers.uno.domain.Card;
+import com.progamers.uno.domain.Colour;
+import com.progamers.uno.domain.Player;
+import com.progamers.uno.domain.Value;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTests {
+    Player player;
+
+    @BeforeEach
+    void setUp() {
+        player = new Player();
+    }
+
+    @Test
+    void startingHandIsCorrectSize() {
+        player.drawStartingHand(7);
+
+        assertEquals(7, player.getHandSize());
+    }
+
+    @Test
+    void addCardToHandingOneElements() {
+        Card card = Card.builder().value(Value.Five).colour(Colour.Red).build();
+        player.addCardToHand(card);
+
+        assertEquals(1, player.getPlayerHand().size());
+    }
+
+    @Test
+    void addCardToHandingFiveElements() {
+        Card card = Card.builder().value(Value.Five).colour(Colour.Red).build();
+        player.addCardToHand(card);
+        player.addCardToHand(card);
+        player.addCardToHand(card);
+        player.addCardToHand(card);
+        player.addCardToHand(card);
+
+        assertEquals(5, player.getPlayerHand().size());
+    }
+
+    @Test
+    void HandIsEmpty() {
+        assertEquals(0, player.getPlayerHand().size());
+    }
+
+    @Test
+    void invalidIndexIsHandled() {
+        player.drawStartingHand(3);
+        try {
+            player.getCurrentSelectedCard(-1);
+        } catch (Exception e) {
+            assertEquals("Index is out of bounds", e.getMessage());
+        }
+        try {
+            player.getCurrentSelectedCard(99);
+        } catch (Exception e) {
+            assertEquals("Index is out of bounds", e.getMessage());
+        }
+    }
+
+    @Test
+    void addCardToHandAndGetCertainCards() {
+        Card cardRedFive = Card.builder().value(Value.Five).colour(Colour.Red).build();
+        player.addCardToHand(cardRedFive);
+
+        Card cardNineBlue = Card.builder().value(Value.Nine).colour(Colour.Blue).build();
+        player.addCardToHand(cardNineBlue);
+
+        assertDoesNotThrow(() -> {
+            Card card = player.getCurrentSelectedCard(0);
+            assertEquals(cardRedFive, card);
+        });
+        assertDoesNotThrow(() -> {
+            Card card = player.getCurrentSelectedCard(1);
+            assertEquals(cardNineBlue, card);
+        });
+    }
+
+//    @Test
+//    void TestToString() {
+//        player.addCardToHand("Red 5");
+//        player.addCardToHand("Blue 5");
+//        player.setCurrentSelected(2);
+//
+//        String expected = "PlayerController{playerHand=[Red 5, Blue 5], currentSelected=2}";
+//        assertEquals(expected, player.toString());
+//        assertEquals("Index is out of bounds", player.getCurrentSelectedCard(player.getCurrentSelected()));
+//    }
+
+    @Test
+    void TestUnoFalse() {
+        Card cardRedFive = Card.builder().value(Value.Five).colour(Colour.Red).build();
+        player.addCardToHand(cardRedFive);
+
+        Card cardNineBlue = Card.builder().value(Value.Nine).colour(Colour.Blue).build();
+        player.addCardToHand(cardNineBlue);
+
+        assertEquals(false, player.getHasUno());
+    }
+
+    @Test
+    void TestUnoTrue() {
+        Card cardRedFive = Card.builder().value(Value.Five).colour(Colour.Red).build();
+        player.addCardToHand(cardRedFive);
+
+        player.DeclareUno();
+
+        assertEquals(true, player.getHasUno());
+    }
+
+    @Test
+    void TestUnoResetsToFalse() {
+        Card cardRedFive = Card.builder().value(Value.Five).colour(Colour.Red).build();
+        player.addCardToHand(cardRedFive);
+
+        player.DeclareUno();
+
+        Card cardBlueFive = Card.builder().value(Value.Five).colour(Colour.Blue).build();
+        player.addCardToHand(cardBlueFive);
+
+        assertEquals(false, player.getHasUno());
+    }
+}
