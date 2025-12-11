89345e3e58889d41e5b6f08e5969ea7079da52c1
Tina Lauro Pollock <christinalauro@hotmail.co.uk>
2025-12-11 15:34:50 +0000

    Rebasing turnorder as part of multiplayer branch # Conflicts: #	src/main/java/com/progamers/uno/domain/game/Game.java

---
+++ src/main/java/com/progamers/uno/domain/game/ValidityChecker.java
@@ -0,0 +1,28 @@
+package com.progamers.uno.domain.game;
+
+import com.progamers.uno.domain.Card;
+import com.progamers.uno.domain.Colour;
+import com.progamers.uno.domain.Value;
+
+public class ValidityChecker {
+    public static boolean isValid(Card topCard, Card selectedCard) {
+        Colour playableColour = topCard.getColour();
+        Value playableValue = topCard.getValue();
+        if(playableColour == Colour.Wild) {
+            playableColour = pickColour(1);
+        }
+        if(playableColour == selectedCard.getColour()) {return true;}
+        else if(playableValue == selectedCard.getValue()) {return true;}
+        else if(selectedCard.getColour() == Colour.Wild) {return true;}
+        return false;
+}
+
+public static Colour pickColour(int userChoice) {
+        if (userChoice == 1){return Colour.Red;}
+        else if (userChoice == 2){return Colour.Blue;}
+        else if (userChoice == 3){return Colour.Green;}
+        else if (userChoice == 4){return Colour.Yellow;}
+        else {System.out.println("Invalid colour");}
+        return Colour.Wild;
+}
+}
