2ac0cd90f30dfdc96fd84740ce46695d8b8a98b3
Blake Harris <dev.blake.harris@gmail.com>
2025-12-15 18:29:53 +0000

    Adds handler to start game

--- src/main/java/com/progamers/uno/controller/LobbyWebSocketController.java
+++ src/main/java/com/progamers/uno/controller/LobbyWebSocketController.java
@@ -2,6 +2,7 @@ package com.progamers.uno.controller;
 
 import com.progamers.uno.domain.multiplayer.lobby.LobbySnapshot;
 import com.progamers.uno.domain.multiplayer.lobby.dto.JoinRequestDTO;
+import com.progamers.uno.domain.multiplayer.lobby.dto.StartGameRequestDTO;
 import com.progamers.uno.service.LobbyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,7 +30,19 @@ public class LobbyWebSocketController {
     @MessageMapping("/lobby/join")
     public void joinLobby(JoinRequestDTO requestDTO, SimpMessageHeaderAccessor headers) {
         String sessionId = headers.getSessionId();
-        LobbySnapshot snapshot = lobbyService.joinLobby(requestDTO.getToken(), requestDTO.getPlayerId(), sessionId);
+        LobbySnapshot snapshot = lobbyService.joinLobby(requestDTO.getToken(), requestDTO.getPlayerName(), requestDTO.getPlayerId(), sessionId);
         messagingTemplate.convertAndSend("/topic/lobby", snapshot);
+    }
+
+    @MessageMapping("/lobby/start")
+    public void start(StartGameRequestDTO requestDTO) {
+        LobbySnapshot snap = lobbyService.startGame(requestDTO.getToken());
+        messagingTemplate.convertAndSend("/topic/lobby", snap);
+        messagingTemplate.convertAndSend("/topic/game/events", "STARTED");
+    }
+
+    @MessageMapping("/lobby/status")
+    public void lobbyStatus() {
+        messagingTemplate.convertAndSend("/topic/lobby", lobbyService.getLobbyStatus());
     }
 }
