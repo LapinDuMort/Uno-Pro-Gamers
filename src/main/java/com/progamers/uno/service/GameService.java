db81131559ab63ffd5c6039078b2c8e4a157d758
Tina Lauro Pollock <christinalauro@hotmail.co.uk>
2025-12-15 14:18:07 +0000

    added validation for playerSelect

--- src/main/java/com/progamers/uno/service/GameService.java
+++ src/main/java/com/progamers/uno/service/GameService.java
@@ -6,11 +6,9 @@ import com.progamers.uno.domain.cards.Card;
 import com.progamers.uno.domain.game.Game;
 import lombok.Getter;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.Scanner;
 
 @Service
@@ -55,37 +53,17 @@ public class GameService {
         Scanner scanner = new Scanner(System.in);
         int playersInGame;
         while (true) {
-            System.out.println("Select number of players (2-8): ");
+            System.out.println("Select number of players (2-8)");
             playersInGame = scanner.nextInt();
             if (playersInGame >= 2 && playersInGame <= 8) {
                 break;
             }
         }
         for (int i = 1; i <= playersInGame; i++) {
-            boolean isBot;
-            while(true){
-                Scanner botScanner = new Scanner(System.in);
-                System.out.println("Is this player a bot? (y/n): ");
-                String createBotPlayer = botScanner.next();
-                if (createBotPlayer.equalsIgnoreCase("y")){
-                    isBot = true;
-                    break;
-                }
-                else if(createBotPlayer.equalsIgnoreCase("n")){
-                    isBot = false;
-                    break;
-                }
-            }
-            String playerName;
-            if (!isBot){
-                Scanner nameScanner = new Scanner(System.in);
-                System.out.println("Enter player " + (i) + "'s name: ");
-                playerName = nameScanner.next();
-            }
-            else{
-                playerName = "Bot " + i;
-            }
-            Player player = new Player(i, playerName, isBot);
+            Scanner nameScanner = new Scanner(System.in);
+            System.out.println("Enter player " + (i) + "'s name:");
+            String playerName = nameScanner.next();
+            Player player = new Player(i, playerName);
             addPlayers(player);
         }
         this.numberOfPlayers = playersInGame;
@@ -118,23 +96,6 @@ public class GameService {
         this.game.drawCards(this.activePlayer, 1);
         this.turnTracker = this.game.incrementTurn(this.turnTracker, this.numberOfPlayers, this.isReverse);
     }
-
-
-    public String checkTopDiscardWild() {
-
-        if(getGame().getDiscardPile().WildColour != null){
-            return getGame().getDiscardPile().WildColour;
-        }
-        return "None";
-    }
-
-    public void playCard(int index, String WildColor) throws Exception {
-        playCard(index);
-        if(WildColor != null) {
-            this.game.getDiscardPile().setWildColour(WildColor);
-        }
-    }
-
 
     public void playCard(int index) throws Exception {
         if (gameOver) return;
