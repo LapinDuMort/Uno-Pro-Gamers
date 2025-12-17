716f27a3bc19a5286aa9b1dff8b4760b2066b86d
blaharris <dev.blake.harris@gmail.com>
2025-12-17 11:03:20 +0000

    Adds DTO test classes

---
+++ src/test/java/com/progamers/uno/domain/multiplayer/game/dto/ActionBaseDTOTests.java
@@ -0,0 +1,16 @@
+package com.progamers.uno.domain.multiplayer.game.dto;
+
+import org.junit.jupiter.api.Test;
+
+public class ActionBaseDTOTests {
+
+    @Test
+    void testConstructor_whenCalled_thenSetsFields() {
+        ActionBaseDTO action = new ActionBaseDTO();
+        action.setToken("sampleToken");
+        action.setPlayerId("player123");
+
+        assert action.getToken().equals("sampleToken");
+        assert action.getPlayerId().equals("player123");
+    }
+}
